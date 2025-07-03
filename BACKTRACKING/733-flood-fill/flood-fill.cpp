f4c831c98cc6d348cb0a86a0f667ca3d3c77bcf8
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-03 11:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (49.76%) - LeetSync

--- BACKTRACKING/733-flood-fill/flood-fill.cpp
+++ BACKTRACKING/733-flood-fill/flood-fill.cpp
@@ -5,16 +5,16 @@ public:
     int dy[4] = {1, -1, 0, 0};
     void dfs(vector<vector<int>>& image, int i, int j, int color,
              int orignal_color, vector<vector<int>>& visited) {
-        if (i < 0 || j < 0 || i >= r || j >= c || orignal_color != image[i][j]) return; 
+        if (i < 0 || j < 0 || i >= r || j >= c || orignal_color != image[i][j] || visited[i][j]) return; 
         image[i][j] =color;
         visited[i][j]=1;
 
         for(int k=0;k<4;k++){
             int newi=i+dx[k];
             int newj=j+dy[k];
-            
+            if (newi >= 0 && newj >= 0 && newi < r && newj < c && !visited[newi][newj]){
                 dfs(image,newi,newj,color,orignal_color,visited);
-            
+            }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
