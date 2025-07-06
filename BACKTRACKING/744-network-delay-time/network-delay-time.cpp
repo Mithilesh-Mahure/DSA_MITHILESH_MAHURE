3db45e566c4669223fb645b0228d5e99d5eed803
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-06 14:18:19 +0530

    Time: 87 ms (50.44%) | Memory: 48.1 MB (18.24%) - LeetSync

---
+++ BACKTRACKING/744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    typedef pair<int,int> pp;
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pp>> adj(n+1);
+        vector<int> distances(n+1,INT_MAX);
+        for(auto x:times){
+            adj[x[0]].push_back({x[1],x[2]});
+        }
+        priority_queue<pp,vector<pp>,greater<pp>> pq;
+        distances[k]=0;
+        pq.push({0,k});
+        while(!pq.empty()){
+            auto f=pq.top();
+            pq.pop();
+            auto node=f.second,dist_node=f.first;
+            for(auto &nbr:adj[node]){
+                auto nbr_node=nbr.first;
+                auto nbr_dist=nbr.second;
+                int prev_dist=distances[nbr_node];
+                int curr_dist=nbr_dist+dist_node;
+
+                if(curr_dist<prev_dist){
+                    distances[nbr_node]=curr_dist;
+                    pq.push({curr_dist,nbr_node});
+                }
+            }
+        }
+        int mx=0;
+        for(int i=1;i<=n;i++){
+            if(distances[i]==INT_MAX) return -1;
+            mx=max(mx,distances[i]);
+            
+        }
+        return mx;
+    }
+};
