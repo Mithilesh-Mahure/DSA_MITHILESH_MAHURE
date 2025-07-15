0a2a875a3debc27ad3752215302af8d6d6e0104c
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-15 18:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.98%) - LeetSync

--- BACKTRACKING/70-climbing-stairs/climbing-stairs.cpp
+++ BACKTRACKING/70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,18 @@
 class Solution {
-    int t[47];
 public:
     int climbStairs(int n) {
-        t[n];
-        if(n==1||n==0){
+        vector<int> dp(n+1);
+        if(n==0||n==1){
             return 1;
         }
-        t[0]=1;
-        t[1]=1;
+
+        dp[0]=1;
+        dp[1]=1;
+
         for(int i=2;i<=n;i++){
-            t[i]=t[i-1]+t[i-2];
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return t[n];
+        return dp[n];
+
     }
 };
