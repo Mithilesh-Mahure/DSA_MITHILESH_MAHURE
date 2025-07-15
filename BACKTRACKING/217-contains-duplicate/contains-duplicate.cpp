450fd40bb1f16ea3361f5fd7038b7c85766a2f2e
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-15 10:15:08 +0530

    Time: 65 ms (22.16%) | Memory: 92.8 MB (18.66%) - LeetSync

--- BACKTRACKING/217-contains-duplicate/contains-duplicate.cpp
+++ BACKTRACKING/217-contains-duplicate/contains-duplicate.cpp
@@ -4,8 +4,11 @@ public:
         unordered_map<int,int> mp;
         for(auto x:nums){
             mp[x]++;
-            if(mp[x]>1) return true;
         }
+        for(auto x:mp){
+            if(x.second>1) return true;
+        }
+
         return false;
     }
 };
