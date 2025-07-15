31b979fd14d00e746de3babbb24acf0848feff40
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-15 10:15:59 +0530

    Time: 82 ms (15.22%) | Memory: 90.9 MB (46.32%) - LeetSync

--- BACKTRACKING/217-contains-duplicate/contains-duplicate.cpp
+++ BACKTRACKING/217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         unordered_map<int,int> mp;
         for(auto x:nums){
             mp[x]++;
+            if(mp[x]>1) return true;
         }
-        for(auto x:mp){
-            if(x.second>1) return true;
-        }
-
         return false;
     }
 };
