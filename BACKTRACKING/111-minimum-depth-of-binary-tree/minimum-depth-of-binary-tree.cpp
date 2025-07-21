2616efc49b9ed748835be911479794350bfc9156
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-21 10:03:43 +0530

    Time: 2 ms (59.67%) | Memory: 147 MB (23.32%) - LeetSync

--- BACKTRACKING/111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ BACKTRACKING/111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -15,14 +15,12 @@ public:
         if(root==NULL){
             return 0;
         }
-        int left=minDepth(root->left);
-        int right=minDepth(root->right);
-        if(left==0){
-            return 1+right;
+        if(!root->left){
+            return 1+minDepth(root->right);
         }
-        if(right==0){
-            return 1+left;
+        if(!root->right){
+            return 1+minDepth(root->left);
         }
-        return 1+min(left,right);
+        return 1+min(+minDepth(root->left),minDepth(root->right));
     }
 };
