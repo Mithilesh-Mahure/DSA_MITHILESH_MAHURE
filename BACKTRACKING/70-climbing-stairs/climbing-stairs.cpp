23946c437d5f36d13b5fde0379a235371ae51ff5
Mithilesh-Mahure <mithileshmlbb@gmail.com>
2025-07-22 20:07:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.78%) - LeetSync

--- BACKTRACKING/70-climbing-stairs/climbing-stairs.cpp
+++ BACKTRACKING/70-climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0||n==1) return 1;
 
-        vector<int> dp(n+1,0);
+        vector<int> dp(n+1);
         dp[0]=1;
         dp[1]=1;
 
